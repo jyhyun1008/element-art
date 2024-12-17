47518bb4fb1f789bd3b53946fbdd2e638f737a61
jyhyun1008 <jayhyun1008@gmail.com>
2024-12-17 19:11:46 +0900

    꼼수?

--- scripts/docker-package.sh
+++ scripts/docker-package.sh
@@ -10,7 +10,7 @@ DIR=$(dirname "$0")
 # If the branch comes out as HEAD then we're probably checked out to a tag, so if the thing is *not*
 # coming out as HEAD then we're on a branch. When we're on a branch, we want to resolve ourselves to
 # a few SHAs rather than a version.
-if [[ $BRANCH != HEAD && ! $BRANCH =~ heads/v.+ ]]
+if [[ $BRANCH != "master" ]]
 then
     DIST_VERSION=$("$DIR"/get-version-from-git.sh)
 fi
